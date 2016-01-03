6f44963fd40e58a48c9462af04155ed3701bd8ff
JustinPhung <justinbao@hotmail.de>
2016-01-03 15:01:26 +0100

    add tag to location

--- src/main/webapp/scripts/generateEntity.js
+++ src/main/webapp/scripts/generateEntity.js
@@ -1,83 +1,105 @@
 /**
  * Created by jp on 08.12.15.
  */
-cityApp.controller( 'generateCtrl', ['$scope', '$http', '$routeParams',
-                                                  function ( $scope, $http, $routeParams ) {
+cityApp.controller('generateCtrl', ['$scope', '$http', '$routeParams',
+    function ($scope, $http, $routeParams) {
 
 
-                                                      $scope.generateCity= function(){
+        $scope.generateCity = function () {
 
 
-                                                          $http.post('../service/city/' + $scope.cityName  ).success( function ( data ) {
+            $http.post('../service/city/' + $scope.cityName).success(function (data) {
 
-                                                              location.reload();
+                location.reload();
 
-                                                          } );
+            });
 
-                                                      }
+        }
 
-                                                      $scope.generateLocation= function(){
-
-
-                                                          $http.post('../service/location/create/'
-                                                                    + $scope.locationName +'/'
-                                                                    + $scope.locationCityId +'/'
-                                                                    + $scope.locationStreet +'/'
-                                                                    + $scope.locationPhonenumber +'/'
-                                                                    + $scope.locationGPS +'/'
-                                                                    + $scope.locationDescription +'/'
-                                                                    + $scope.locationHousenumber ).success( function ( data ) {
-
-                                                              location.reload();
-
-                                                          } );
-
-                                                      }
-
-                                                      $scope.generatePost= function(){
+        $scope.generateLocation = function () {
 
 
-                                                          $http.post('../service/location/'
-                                                              + $scope.postUserId +'/'
-                                                              + $scope.postLocationId +'/'
-                                                              + $scope.postContent +'/').success( function ( data ) {
+            $http.post('../service/location/create/'
+                + $scope.locationName + '/'
+                + $scope.locationCityId + '/'
+                + $scope.locationStreet + '/'
+                + $scope.locationPhonenumber + '/'
+                + $scope.locationGPS + '/'
+                + $scope.locationDescription + '/'
+                + $scope.locationHousenumber).success(function (data) {
 
-                                                              location.reload();
+                location.reload();
 
-                                                          } );
+            });
 
-                                                      }
+        }
 
-                                                      $scope.likePost= function(){
-
-                                                          $http.put('../service/event/'
-                                                              + $scope.likeEventId + '/'
-                                                              + $scope.likeUserId + '/', 'true').success( function ( data ) {
-
-                                                              location.reload();
-                                                          });
-
-                                                      }
+        $scope.generatePost = function () {
 
 
-                                                      $scope.generateComment= function(){
+            $http.post('../service/location/'
+                + $scope.postUserId + '/'
+                + $scope.postLocationId + '/'
+                + $scope.postContent + '/').success(function (data) {
 
-                                                          $http.post('../service/event/'
-                                                              + $scope.commentEventId + '/'
-                                                              + $scope.commentUserId, $scope.commentContent).success( function ( data ) {
+                location.reload();
 
-                                                              location.reload();
-                                                          });
-                                                      }
+            });
 
-                                                      $scope.generateTag= function(){
+        }
 
-                                                          $http.post('../service/tag/'
-                                                              + $scope.tagName).success( function ( data ) {
+        $scope.likePost = function () {
 
-                                                              location.reload();
-                                                          });
-                                                      }
+            $http.put('../service/event/'
+                + $scope.likeEventId + '/'
+                + $scope.likeUserId + '/', 'true').success(function (data) {
+
+                location.reload();
+            });
+
+        }
 
 
-                                                  }] );
+        $scope.generateComment = function () {
+
+            $http.post('../service/event/'
+                + $scope.commentEventId + '/'
+                + $scope.commentUserId, $scope.commentContent).success(function (data) {
+
+                location.reload();
+            });
+        }
+
+        $scope.generateTag = function () {
+
+            $http.post('../service/tag/'
+                + $scope.tagName).success(function (data) {
+
+                location.reload();
+            });
+        }
+
+        $scope.addTagToLocation = function () {
+
+            $http.post('../service/location/'
+                + $scope.locationId + '/'
+                + $scope.tagId + '/addTag').success(function (data) {
+
+                location.reload();
+            });
+
+        }
+
+        $scope.removeTagFromLocation = function () {
+
+            $http.post('../service/location/'
+                + $scope.locationId + '/'
+                + $scope.tagId + '/removeTag').success(function (data) {
+
+                location.reload();
+            });
+
+        }
+
+
+    }]);
